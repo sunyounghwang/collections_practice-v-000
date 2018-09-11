30e639d39f0a1077b8337215181b5ec6056f7b6a
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-11 20:16:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,5 +6,11 @@ def sort_array_desc(array)
   array.sort.reverse
 end
 
-def sort_array_char_count(array)
+def sort_array_char_count(array) 
+  i = 0              
+  while i < array.size                        
+    yield(array[i])               
+    i += 1
+  end
+  array                           
 end
